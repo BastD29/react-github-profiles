08e38317e92e33caf8f9b77f17bcf83ed88faa01
BastD29 <bastiendaniel9@gmail.com>
2024-07-11 11:53:27 +0200

    first commit - added sass

---
+++ src/components/App/App.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+import style from "./App.module.scss";
+const App: FC = () => {
+  return (
+    <div className={style["app"]}>
+      <h2>App</h2>
+    </div>
+  );
+};
+
+export default App;
