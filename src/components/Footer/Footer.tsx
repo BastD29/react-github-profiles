4db268326c242b7d1b708e5b96ec874564bf02c0
BastD29 <bastiendaniel9@gmail.com>
2024-07-17 16:41:52 +0200

    added responsive design for footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,9 +4,7 @@ import style from "./Footer.module.scss";
 const Footer: FC = () => {
   return (
     <footer className={style["footer"]}>
-      <p className="copyright">
-        © {new Date().getFullYear()} Bastien Daniel. All rights reserved.
-      </p>
+      <p>© {new Date().getFullYear()} Bastien Daniel. All rights reserved.</p>
     </footer>
   );
 };
